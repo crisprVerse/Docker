0d5e7254dda4b71674b156397aa5afcd9f0e463d
fortinj2 <fortin.jeanphilippe@gene.com>
2022-08-30 11:25:11 -0700

    Modified internal

--- internal.sh
+++ internal.sh
@@ -3,10 +3,10 @@ docker tag crisprverse fortin946/crisprverse:dev
 docker push fortin946/crisprverse:dev
 
 # To enter the container:
-docker run -it crisprverse /bin/bash
+docker run -it fortin946/crisprverse:dev /bin/bash
 
 # To start R directly from the container:
-docker run -it crisprverse
+docker run -it fortin946/crisprverse:dev
 
 # To mount a local directory:
-docker run -it -v '/Users/fortinj2/crisprIndices:/home/crisprverse/indices' crisprverse
+docker run -it -v '/Users/fortinj2/crisprIndices:/home/crisprverse/indices' fortin946/crisprverse:dev
